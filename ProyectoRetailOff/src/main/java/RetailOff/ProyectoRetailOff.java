59ea7e891430e228cc9038fb362def0212ea3ae0
KeldarWolf <144638401+KeldarWolf@users.noreply.github.com>
2024-12-22 18:19:22 -0300

    Add files via upload

---
+++ ProyectoRetailOff/src/main/java/RetailOff/ProyectoRetailOff.java
@@ -0,0 +1,12 @@
+package RetailOff;
+
+public class ProyectoRetailOff {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Bienvenidoo al Proyecto RetailOff"); 
+		
+	}
+
+}
