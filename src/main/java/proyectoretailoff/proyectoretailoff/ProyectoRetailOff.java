01699a0b7f8009da6f6a56a097564c66e57f7561
KeldarWolf <144638401+KeldarWolf@users.noreply.github.com>
2024-12-23 00:48:09 -0300

    Add files via upload

---
+++ src/main/java/proyectoretailoff/proyectoretailoff/ProyectoRetailOff.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proyectoretailoff.proyectoretailoff;
+
+/**
+ *
+ * @author Keldar
+ */
+public class ProyectoRetailOff {
+
+    public static void main(String[] args) {
+        
+        	System.out.println("Bienvenidoo al Proyecto RetailOff"); 
+    }
+}
